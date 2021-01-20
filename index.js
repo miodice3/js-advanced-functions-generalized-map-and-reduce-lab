937dfbe9da446220da2e27cf092669e06a4ab305
Michael Iodice <miodice3@gmail.com>
2021-01-20 16:11:58 -0700

    commit tree c3ec13b6f37859e7c855581d1b502773f228aea7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(ray, doThis){
+    return ray.map(function(e){
+        return doThis(e)
+    })
+}
+
+// function map(ray, doThis){
+//     return ray.map(e => doThis(e) )
+// }
+
+// function map(ray, cb){
+//     return ray.map(e => cb(e))
+// }
+
+function reduce(ray, cb, startingPoint){
+    return ray.reduce(cb, startingPoint)
+}
+
+
+// function reduce(ray, startingPoint){
+//     return ray.reduce(function cb(){
+
+//     }, startingPoint=0)
+//     }
+// }
